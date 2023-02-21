fa88eeaad9ced4cc6376e3d4b00936d0a84ef7b4
Modupe-Favour <modups05@gmail.com>
2023-02-21 05:08:45 -0800

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "favour.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	_putchar('F');
+	_putchar('A');
+	_putchar('V');
+	_putchar('O');
+	_putchar('U');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
