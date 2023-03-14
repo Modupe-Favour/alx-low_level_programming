0b6693a6418c24955d529973f103a01a5c984f63
Modupe-Favour <modups05@gmail.com>
2023-03-14 13:59:49 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,83 +1,36 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
+
 /**
-		
- * str_concat - get ends of input and add together for size
-		
- * @s1: input one to concat
-		
- * @s2: input two to concat
-		
- * Return: concat of s1 and s2
-		
+ * str_concat - Concatenates two strings.
+ * @s1: string to be concatenated on.
+ * @s2: The string to be concatenated to s1.
+ * Return: NULL on failure, otherwise, the newly concatenated string
  */
-		
 char *str_concat(char *s1, char *s2)
-		
 {
-		
-	char *conct;
-		
-	int i, ci;
-		
+	char *concat_str;
+	int con, concat_con = 0, len = 0;
 
-		
 	if (s1 == NULL)
-		
 		s1 = "";
-		
+
 	if (s2 == NULL)
-		
 		s2 = "";
-		
 
-		
-		i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-		i++;
-		
-	while (s2[ci] != '\0')
-		
-		ci++;
-		
-	conct = malloc(sizeof(char) * (i + ci + 1));
-		
+	for (con = 0; s1[con] || s2[con]; con++)
+		len++;
 
-		
-	if (conct == NULL)
-		
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
 		return (NULL);
-		
-	i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-	{
-		
-		conct[i] = s1[i];
-		
-		i++;
-		
-	}
-		
 
-		
-	while (s2[ci] != '\0')
-		
-	{
-		
-		conct[i] = s2[ci];
-		
-		i++, ci++;
-		
-	}
-		
-	conct[i] = '\0';
-		
-	return (conct);
-		
+	for (con = 0; s1[con]; con++)
+		concat_str[concat_con++] = s1[con];
+
+	for (con = 0; s2[con]; con++)
+		concat_str[concat_con++] = s2[con];
+
+	return (concat_str);
 }
